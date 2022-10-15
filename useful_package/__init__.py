2c055c386506918ca1abbb19e2d59c82988ee672
NKashin <nikolay1s112@gmail.com>
2022-10-16 00:56:19 +0300

    add module a

--- useful_package/__init__.py
+++ useful_package/__init__.py
@@ -1,1 +1,6 @@
 #some code here
+from module_a import polynom_3
+
+if __name__ == '__main__':
+    print('hi, in init')
+    print(f'4 by 4 is {polynom_3(4)}')
---
+++ useful_package/module_a.py
@@ -0,0 +1,9 @@
+"""
+Returns cubic
+"""
+
+def polynom_3 (x):
+    return x**3
+
+if __name__ == '__main__':
+    print(f'hi, polynom 3 by 3 is {polynom_3(3)}')
